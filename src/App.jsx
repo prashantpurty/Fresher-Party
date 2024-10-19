0fbc4ce991e41a1c070d9b0cd97b126620c6f013
Prashant Purty <prashantpurty17@gmail.com>
2024-10-19 20:51:43 +0530

    fresher party

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BackgroundLinesDemo } from './components/BackgroundLinesDemo'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <BackgroundLinesDemo />
+    </>
+  )
+}
+
+export default App
